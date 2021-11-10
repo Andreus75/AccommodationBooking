ffbf9ada592e14ca4a49b6642e8bf8f0b76f049c
Andreus75 <t_andriy@ukr.net>
2021-11-10 18:52:21 +0200

    add Postman file

--- util/default-data.utils.js
+++ util/default-data.utils.js
@@ -1,12 +1,15 @@
 const User = require('../dataBase/User');
 const { DEFAULT_PASSWORD } = require('../configs/config');
-const { passwordService } = require('../services');
+const { passwordService, emailService, jwtService} = require('../services');
 const { ADMIN } = require('../configs/user_role_enum');
+const { WELCOME } = require('../configs/email_action_enum');
+const Action = require('../dataBase/Action');
+const { ACTION } = require('../configs/token_type_enum');
 
 module.exports = async () => {
     const hashedPassword = await passwordService.hash(DEFAULT_PASSWORD);
 
-    await User.create({
+    const newAdmin = await User.create({
         name: 'Anna',
         surname: 'Koolya',
         age: 25,
@@ -14,4 +17,8 @@ module.exports = async () => {
         email: 'Anna.admin@gmail.com',
         password: hashedPassword,
     });
+    const token = jwtService.createActivateToken();
+
+    await Action.create({ token, type: ACTION, user_id: newAdmin._id });
+    await emailService.sendMail(newAdmin.email, WELCOME, { userName: name, token });
 };
